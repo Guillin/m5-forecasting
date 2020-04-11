bca13242339beeee2144cf8fa4c1cbdb4b4eb2e2
Guillin <guillermo.lissa@gmail.com>
2020-04-11 15:11:36 +0000

    build submit was modified an replace most of their function for loops

--- notebooks/build_submit.sh
+++ notebooks/build_submit.sh
@@ -1,14 +1,31 @@
 #!/bin/bash
-FILE1 = data/make_dataset
-FILE2 = features/build_features
-FILE3 = data/time_series_split
-FILE4 = models/light_gbm/train_lightGBM_regressor
-FILE5 = models/light_gbm/test_lightGBM_regressor
+FILE1="data/make_dataset"
+FILE2="features/build_features"
+FILE3="data/time_series_split"
+FILE4="models/light_gbm/train_lightGBM_regressor"
+FILE5="models/light_gbm/test_lightGBM_regressor"
+
+SUBMIT="submit_001.py"
+
+echo "INIT"
+## declare an array variable
+declare -a scripts=($FILE1 $FILE2 $FILE3 $FILE4 $FILE5)
 
 
-jupyter nbconvert $FILE1.ipynb --to python
-jupyter nbconvert $FILE2.ipynb --to python 
-jupyter nbconvert $FILE3.ipynb --to python
-jupyter nbconvert $FILE4.ipynb --to python
-jupyter nbconvert $FILE5.ipynb --to python
-cat $FILE1 $FILE2 $FILE3 $FILE4 $FILE5 > submit_001.py
+## now loop through the above array
+for i in "${scripts[@]}"
+do
+	echo "Converting $i to py"
+	jupyter nbconvert $i.ipynb --to python
+done
+
+echo "Concatenating files in main script submit.py"
+FINAL_SCRIPT=""
+for i in "${scripts[@]}"
+do
+	FINAL_SCRIPT+=" $i.py"
+done
+
+cat $FINAL_SCRIPT > $SUBMIT
+
+echo "DONE"
